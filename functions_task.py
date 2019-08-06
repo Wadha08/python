5e829ef1ef609c54ee85460e0abeb4c695a961e2
wadha08 <wadha-ra@hotmail.com>
2019-08-06 13:53:34 +0300

    changing to 4-space indentations

--- functions_task.py
+++ functions_task.py
@@ -6,66 +6,59 @@ DayOfbirth = input("Enter day of birth: ")
 
 
 def check_birthdate(yearOfbirth ,MonthOfbirth,DayOfbirth):#check if the dateBirth valid or not
- Date = (yearOfbirth+"-"+MonthOfbirth+"-"+DayOfbirth) #convert the string to date format
- DateBirth = datetime.strptime(Date, "%Y-%m-%d") #convert the string to datetime object
- currentDate = datetime.now()
- if currentDate.date() > DateBirth.date():
-  return calculate_age(yearOfbirth,MonthOfbirth,DayOfbirth)
- else:
-  return print("the birthdate is invalid.")
+    Date = (yearOfbirth+"-"+MonthOfbirth+"-"+DayOfbirth) #convert the string to date format
+    DateBirth = datetime.strptime(Date, "%Y-%m-%d") #convert the string to datetime object
+    currentDate = datetime.now()
+    if currentDate.date() > DateBirth.date():
+        return calculate_age(yearOfbirth,MonthOfbirth,DayOfbirth)
+    else:
+        return print("the birthdate is invalid.")
 
 def calculate_age(yearOfbirth ,MonthOfbirth,DayOfbirth):
- Date = (yearOfbirth+"-"+MonthOfbirth+"-"+DayOfbirth) #convert the string to date format
- DateBirth = datetime.strptime(Date, "%Y-%m-%d") #convert the string to datetime object
- currentDate = datetime.now()
+    Date = (yearOfbirth+"-"+MonthOfbirth+"-"+DayOfbirth) #convert the string to date format
+    DateBirth = datetime.strptime(Date, "%Y-%m-%d") #convert the string to datetime object
+    currentDate = datetime.now()
 
- #Calculate Days seperatly 
- Daybirth = DateBirth.day
- Daycurrent = currentDate.day
+    #Calculate Days seperatly 
+    Daybirth = DateBirth.day
+    Daycurrent = currentDate.day
 
-#Calculate Month seperatly 
- MonthOfbirth = DateBirth.month
- Monthcurrent = currentDate.month
+    #Calculate Month seperatly 
+    MonthOfbirth = DateBirth.month
+    Monthcurrent = currentDate.month
 
-#Calculate Years seperatly 
- yearOfbirth = DateBirth.year
- yearcurrent = currentDate.year
+    #Calculate Years seperatly 
+    yearOfbirth = DateBirth.year
+    yearcurrent = currentDate.year
 
 
-#Days left
- if Daycurrent < Daybirth: 
-    Daycurrent = Daycurrent+30
-    Dayleft = Daycurrent-Daybirth
-    Monthcurrent = Monthcurrent-1
+    #Days left
+    if Daycurrent < Daybirth: 
+        Daycurrent = Daycurrent+30
+        Dayleft = Daycurrent-Daybirth
+        Monthcurrent = Monthcurrent-1
 
- else:
-    Dayleft = Daycurrent-Daybirth
+    else:
+        Dayleft = Daycurrent-Daybirth
 
  
 
-#Month left
- if Monthcurrent < MonthOfbirth:
-    Monthcurrent =  Monthcurrent+12
-    MonthLeft = Monthcurrent - MonthOfbirth
-    yearcurrent = yearcurrent-1
-    Monthleft = Monthcurrent - MonthOfbirth
+    #Month left
+    if Monthcurrent < MonthOfbirth:
+        Monthcurrent =  Monthcurrent+12
+        MonthLeft = Monthcurrent - MonthOfbirth
+        yearcurrent = yearcurrent-1
+        Monthleft = Monthcurrent - MonthOfbirth
     
 
- else:
-    Monthleft = Monthcurrent - MonthOfbirth
- #print(Monthleft)
+    else:
+        Monthleft = Monthcurrent - MonthOfbirth
+        #print(Monthleft)
 
 
-#years left 
- yearsleft =yearcurrent-yearOfbirth
- print("You are "+str(yearsleft)+" years, "+str(Monthleft)+" months, and "+str(Dayleft)+" days")
- #print(yearsleft)
-
-
-
-
-
-
-
+        #years left 
+    yearsleft =yearcurrent-yearOfbirth
+    print("You are "+str(yearsleft)+" years, "+str(Monthleft)+" months, and "+str(Dayleft)+" days")
+    #print(yearsleft)
 
 check_birthdate(yearOfbirth,MonthOfbirth,DayOfbirth)
